7c22b91bc2aba6d91ba9fd9a1e0719392bcc870e
Varya Utkina <varya2412@mail.ru>
2024-10-23 10:31:56 +0300

    Setup MainScreen: work with storyboards

--- MovieApp/ViewControllers/MainViewController.swift
+++ MovieApp/ViewControllers/MainViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
+    @IBOutlet var mediumView: UIView!
+    @IBOutlet var bigView: UIView!
+    @IBOutlet var miniView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
