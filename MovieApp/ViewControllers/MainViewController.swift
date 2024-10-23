8c0f47e5315dbe0f3a0a565c559256a1c19df724
Varya Utkina <varya2412@mail.ru>
2024-10-23 10:36:49 +0300

    MainVC: add cornerRadius

--- MovieApp/ViewControllers/MainViewController.swift
+++ MovieApp/ViewControllers/MainViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 final class MainViewController: UIViewController {
 
+    @IBOutlet var miniView: UIView!
     @IBOutlet var mediumView: UIView!
     @IBOutlet var bigView: UIView!
-    @IBOutlet var miniView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        miniView.layer.cornerRadius = miniView.frame.width / 2
+        mediumView.layer.cornerRadius = mediumView.frame.width / 2
+        bigView.layer.cornerRadius = bigView.frame.width / 2
     }
 
 
