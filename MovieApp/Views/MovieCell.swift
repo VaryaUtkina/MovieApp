4e3f6c8be079ebfaad8ef24f44cd0a3e9d1d55c6
Varya Utkina <varya2412@mail.ru>
2024-10-27 20:22:32 +0300

    MovieCell: fetch Image

--- MovieApp/Views/MovieCell.swift
+++ MovieApp/Views/MovieCell.swift
@@ -12,11 +12,26 @@ final class MovieCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var rateLabel: UILabel!
     @IBOutlet var yearAndTypeLabel: UILabel!
-    @IBOutlet var movieView: UIView!
+    @IBOutlet var movieView: UIImageView!
     
     func configure(with movie: Movie) {
         nameLabel.text = movie.title
         rateLabel.text = movie.imdbrating.formatted()
         yearAndTypeLabel.text = "\(movie.released), \(movie.type)"
+        
+        DispatchQueue.global().async {
+            if let imageUrlString = movie.imageurl.first,
+                let url = URL(string: imageUrlString),
+                let imageData = try? Data(contentsOf: url) {
+                DispatchQueue.main.async { [unowned self] in
+                    movieView.image = UIImage(data: imageData)
+                }
+            } else {
+                DispatchQueue.main.async { [unowned self] in
+                    movieView.image = UIImage(systemName: "movieclapper")
+                    movieView.tintColor = .black
+                }
+            }
+        }
     }
 }
